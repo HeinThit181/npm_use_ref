8e8fcb6c73f0008640b0bb4aaf8cfd429bfbb609
HeinThit181 <u6715181@au.edu>
2025-12-26 22:08:31 +0700

    Update App.jsx

--- use_ref/src/App.jsx
+++ use_ref/src/App.jsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Register />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   )
 }
